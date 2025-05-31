36470491c0e53a18899f62d47065c41f630cc311
vlad <infinitiesair@gmail.com>
2025-05-31 10:35:16 +0300

    goit-js-hw-04 - complited

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,15 @@
-function slugify(title) {
-  const lower = title.toLowerCase();
-  const spl = lower.split(" ");
-  return spl.join("-");
+function isEnoughCapacity(products, containerSize) {
+  let total = 0;
+
+  for (let value of Object.values(products)) {
+    total += value;
+  }
+
+  return total <= containerSize;
 }
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
-// function slugify(title) {
-//   return title.toLowerCase().split(" ").join("-");
-// }
+// Залиш цей код для перевірки ментором
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,25 @@
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  for (let i = 0; i < numbers.length; i++) {
-    const number = numbers[i];
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-    if (number > value) {
-      filteredNumbers.push(number);
-    }
-  }
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-  return filteredNumbers;
-}
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+// Залиш цей код для перевірки ментором
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
